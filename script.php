570daef0fd78a90159f2ce274bff9496f8a4ded4
bobozhangshao <1310255342@qq.com>
2015-12-22 15:20:34 +0800

    create folder of devices' pics

---
+++ script.php
@@ -0,0 +1,22 @@
+﻿<?php
+defined('_JEXEC') or die('Restricted acccess');
+
+/**
+ * Script file of HeartCare component
+ */
+class Com_HeartcareInstallerScript
+{
+	/**
+	 * method to install the component
+	 * create devices folder
+	 * @return void
+	 */
+	function install($parent)
+	{
+		$path = JPATH_SITE . '/images/devices';
+
+		if (!JFolder::exists($path)) {
+			JFolder::create($path, 0777);
+		}
+	}
+}
